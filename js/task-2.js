ccaa367cee48dbdf90504d7658f4dbe218918a29
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 11:08:24 -0400

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,26 @@
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message; 
-  } else {
-    return message.slice(0, maxLength) + "..."; 
+document.addEventListener('DOMContentLoaded', () => {
+  function formatMessage(message, maxLength) {
+      if (message.length <= maxLength) {
+          return message;
+      } else {
+          return message.slice(0, maxLength) + "...";
+      }
   }
-}
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); 
-console.log(formatMessage("Curabitur ligula sapien", 23)); 
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
+  const task2Results = document.getElementById('task-2-results');
+
+  const messages = [
+      formatMessage("Curabitur ligula sapien", 16),
+      formatMessage("Curabitur ligula sapien", 23),
+      formatMessage("Vestibulum facilisis purus nec", 20),
+      formatMessage("Vestibulum facilisis purus nec", 30),
+      formatMessage("Nunc sed turpis a felis in nunc fringilla", 15),
+      formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)
+  ];
+
+  messages.forEach(message => {
+      const p = document.createElement('p');
+      p.textContent = message;
+      task2Results.appendChild(p);
+  });
+});
