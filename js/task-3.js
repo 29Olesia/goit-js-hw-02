f1a42b6f3f6bc4aee20ac58ad35b8223293a7fa0
29Olesia <148815850+29Olesia@users.noreply.github.com>
2024-05-25 11:08:41 -0400

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,24 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-}
+document.addEventListener('DOMContentLoaded', () => {
+  function checkForSpam(message) {
+      const lowerCaseMessage = message.toLowerCase();
+      return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+  }
 
-console.log(checkForSpam("Latest technology news")); 
-console.log(checkForSpam("JavaScript weekly newsletter")); 
-console.log(checkForSpam("Get best sale offers now!")); 
-console.log(checkForSpam("Amazing SalE, only tonight!")); 
-console.log(checkForSpam("Trust me, this is not a spam message")); 
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
-console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+  const task3Results = document.getElementById('task-3-results');
+
+  const messages = [
+      checkForSpam("Latest technology news"),
+      checkForSpam("JavaScript weekly newsletter"),
+      checkForSpam("Get best sale offers now!"),
+      checkForSpam("Amazing SalE, only tonight!"),
+      checkForSpam("Trust me, this is not a spam message"),
+      checkForSpam("Get rid of sPaM emails. Our book in on sale!"),
+      checkForSpam("[SPAM] How to earn fast money?")
+  ];
+
+  messages.forEach(message => {
+      const p = document.createElement('p');
+      p.textContent = message;
+      task3Results.appendChild(p);
+  });
+});
